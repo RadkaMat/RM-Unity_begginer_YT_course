3c6bbfe128f7ceed3c329ac4ebac1bb14f33d645
RadkaMat <104318039+RadkaMat@users.noreply.github.com>
2023-02-28 02:27:18 +0100

    Create code_example.cs

---
+++ code_example.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class test : MonoBehaviour
+{
+
+
+    private void Start() {
+        if (transform == transform) {
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {
+                    // Do something
+                }
+            }
+        }
+    }
+
+    private void Update() { 
+    }
+
+
+}
